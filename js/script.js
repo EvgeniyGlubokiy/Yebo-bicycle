1fb29a4e9c0297312590d7dddad91f69d1ca7841
EvgeniyGlubokiy <eglubokiy@gmail.com>
2022-01-26 22:38:55 +0200

    new2

--- js/script.js
+++ js/script.js
@@ -10,8 +10,34 @@ function ibg() {
 
 ibg();
 
+//burger menu====================================
+
+
 $('.icon-menu').click(function (event) {
-    $(this).togleClass('active');
-    $('.menu__body').togleClass('active');
-    $('body').togleClass('lock');
+    $(this).toggleClass('active');
+    $('.menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
 });
+
+//========================================
+
+
+
+//slider===============
+if ($(".slider__body").length > 0) {
+    $(".slider__body").slick({
+        dots: true,
+        arrows: false,
+        accessibility: false,
+        slidesToShow: 1,
+        autoplaySpeed: 3000,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        responsive: [{
+            breakpoint: 768,
+            setings: {}
+        }]
+    });
+}
+
+//=============================================
