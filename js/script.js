dbcf5188c008de7605d364368470d4f3c73b7cf9
EvgeniyGlubokiy <eglubokiy@gmail.com>
2022-01-27 18:38:14 +0200

    new3

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ if ($(".slider__body").length > 0) {
         arrows: false,
         accessibility: false,
         slidesToShow: 1,
+        adaptiveHeight: true,
         autoplaySpeed: 3000,
         nextArrow: '<button type="button" class="slick-next"></button>',
         prevArrow: '<button type="button" class="slick-prev"></button>',
